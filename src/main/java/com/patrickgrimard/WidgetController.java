34dd46b6f7fdedd849eaa46219c87be0cb2afa57
Thijs Lanssens <thijs.lanssens@ida-mediafoundry.be>
2017-11-13 09:13:29 +0100

    Progress

--- src/main/java/com/patrickgrimard/WidgetController.java
+++ src/main/java/com/patrickgrimard/WidgetController.java
@@ -8,12 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by XTL on 8/14/2014.
  */
-@RequestMapping("/api/**")
+@RequestMapping( "/" )
 @RestController
-public class WidgetController {
+public class WidgetController{
 
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Widget index() {
-        return new Widget("green", 10, 7);
+    @RequestMapping( value = "/", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public String index(){ return "Fill in a color in the URI"; }
+
+    @RequestMapping( value = "/green", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public Widget green(){
+        return new Widget( "green", 10, 7 );
+    }
+
+    @RequestMapping( value = "/blue", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public Widget blue(){
+        return new Widget( "blue", 10, 7 );
+    }
+
+    @RequestMapping( value = "/red", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public Widget red(){
+        return new Widget( "red", 10, 7 );
+    }
+
+    @RequestMapping( value = "/yellow", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public Widget yellow(){
+        return new Widget( "yellow", 10, 7 );
     }
 }
